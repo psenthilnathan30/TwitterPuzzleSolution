29204c101173566fc281933b6f328803c044b589
senthil <psenthilnathan30@gmail.com>
2012-08-14 12:39:26 +0530

    Model Modified

--- src/main/java/servlet/com/twitter/model/TwitterClass.java
+++ src/main/java/servlet/com/twitter/model/TwitterClass.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.twitter.model;
+package servlet.com.twitter.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
